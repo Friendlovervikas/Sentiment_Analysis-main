866bfa37b0451dc8110180eb50d9093f7e850d65
VIKAS YADAV <136366317+Friendlovervikas@users.noreply.github.com>
2026-04-30 15:54:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,6 @@ def live_stream():
 
 # ----------------------------- Main -----------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available, otherwise default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
